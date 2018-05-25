306f6704c0602767b163b941eaf2f344b218e457
wind-fire <877400859@qq.com>
2018-05-25 08:00:53 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title','Sample App') -- Laravel 基础教程二代目</title>
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    <h2>主页</h2>
+@endsection
