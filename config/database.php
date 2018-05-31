58c7ddc68dc3e7924a1a8c037f27d06a38fcadc6
wind-fire <877400859@qq.com>
2018-05-31 10:42:07 +0000

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
             'engine' => null,
         ],
 
-        'pgsql' => [
+        /*'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
@@ -66,7 +66,20 @@ return [
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
+        ],*/
+        'pgsql' => [
+            'driver'   => 'pgsql',
+            'host'     => $db_config['host'],
+            'port'     => env('DB_PORT', '5432'),
+            'database' => $db_config['database'],
+            'username' => $db_config['username'],
+            'password' => $db_config['password'],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+            'sslmode'  => 'prefer',
         ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
