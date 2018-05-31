32ac1dc81f40c67d28ab6f25a2c20e5c03791e5e
wind-fire <877400859@qq.com>
2018-05-31 09:51:49 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,13 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    //注册用户页面
     public function  create()
     {
         return view('users.create');
     }
+
+    //返回当前用户信息
+    public function show(User $user)
+    {
+        /*将用户对象 $user 通过 compact 方法转化为一个关联数组，并作为第二个参数传递给 view 方法，将数据与视图进行绑定。*/
+        return view('users.show',compact('user'));
+    }
+
+    public function  store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|min:3|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+
+
+        ]);
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+        /*这里是一个『约定优于配置』的体现，此时 $user 是 User 模型对象的实例。route() 方法会自动获取 Model 的主键，也就是数据表 users 的主键 id，以上代码等同于：
+
+        redirect()->route('users.show', [$user->id]);*/
+
+        return redirect()->route('users.show',[$user]);
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,16 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
-
 Route::get('signup', 'UsersController@create')->name('signup');
+/*resource 方法  相当于以下方法
+路由声明时必须使用 Eloquent 模型的单数小写格式来作为路由片段参数，User 对应 {user}
+Route::get('/users', 'UsersController@index')->name('users.index');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::get('/users/create', 'UsersController@create')->name('users.create');
+Route::post('/users', 'UsersController@store')->name('users.store');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+*/
+Route::resource('users', 'UsersController');
+
